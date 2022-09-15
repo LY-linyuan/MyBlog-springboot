c07b47180f53646cfe5f08cfcc474dce432268e3
LY-linyuan <3113714035@qq.com>
2022-09-16 00:01:24 +0800

    个人博客系统springboot+vue-springboot部分尝试练习中------

---
+++ blog-parent/blog-api/src/main/java/com/tang/blog/BlogApp.java
@@ -0,0 +1,20 @@
+package com.tang.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 临渊
+ * @Date 2022-09-15 23:35
+ */
+
+@SpringBootApplication
+public class BlogApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlogApp.class, args);
+
+    }
+
+}
