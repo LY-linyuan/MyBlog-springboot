c416e3abdc503e3045bc934c71f1daa56031dbd2
LY-linyuan <3113714035@qq.com>
2022-09-22 22:22:34 +0800

    个人博客系统springboot+vue-springboot部分尝试练习中------

---
+++ blog-parent/blog-admin/src/main/java/com/tang/blog/admin/AdminApp.java
@@ -0,0 +1,16 @@
+package com.tang.blog.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 临渊
+ * @Date 2022-09-22 20:50
+ */
+@SpringBootApplication
+public class AdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApp.class,args);
+    }
+}
---
+++ blog-parent/blog-admin/src/main/java/com/tang/blog/admin/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.tang.blog.admin.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author 临渊
+ * @Date 2022-09-22 20:49
+ */
+@Configuration
+@MapperScan("com.tang.blog.admin.mapper")
+public class MybatisPlusConfig {
+
+    //分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
